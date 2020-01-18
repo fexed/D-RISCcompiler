4db0a0e74f79f5bfdbb8ac02c029afde4c1c798d
Federico Matteoni <fedex15@live.it>
2020-01-18 23:46:15 +0100

    First steps pt. 2

--- main.c
+++ main.c
@@ -8,15 +8,15 @@
 */
 
 /*
-	0 ADD
-	1 SUB
+	0 ADD r r r
+	1 SUB r r r
 	2 MUL
 	3 DIV
 	4 AND
 	5 OR
 	6 NOT
-	7 LOAD
-	8 STORE
+	7 LOAD r r r
+	8 STORE r r r
 	9 EXCHANGE
 	10 IF>
 	11 IF<
@@ -32,8 +32,8 @@
 	21 CLEAR
 	22 SHR
 	23 SHL
-	24 ADDI
-	25 SUBI
+	24 ADDI r n r
+	25 SUBI r n r
 */
 int parseCommand(char *buff) {
 	if (strcmp(buff, "ADD") == 0) 		{ return 0; }
@@ -45,14 +45,101 @@ int parseCommand(char *buff) {
 	return -1;
 }
 
+int execCommand(int command, char* params, int* registers) {
+	if (command == 0) { //R1 + R2 -> R3
+		int R1, R2, R3;
+		char* tokens;
+		char* save_ptr;
+		tokens = strtok_r(params, ",", &save_ptr);
+		if (tokens != NULL) { //R1
+			tokens = strchr(tokens, 'R');
+		 	R1 = atoi(++tokens);
+		}
+		tokens = strtok_r(NULL, ",", &save_ptr);
+		if (tokens != NULL) { //R2
+			tokens = strchr(tokens, 'R');
+		 	R2 = atoi(++tokens);
+		}
+		tokens = strtok_r(NULL, ",", &save_ptr);
+		if (tokens != NULL) { //R3
+			tokens = strchr(tokens, 'R');
+		 	R3 = atoi(++tokens);
+		}
+		registers[R3] = registers[R1] + registers[R2];
+		printf("R%d + R%d -> R%d\n", R1, R2, R3);
+	} else if (command == 24) { //R1 + n -> R3
+		int R1, R2, R3;
+		char* tokens;
+		char* save_ptr;
+		tokens = strtok_r(params, ",", &save_ptr);
+		if (tokens != NULL) { //R1
+			tokens = strchr(tokens, 'R');
+		 	R1 = atoi(++tokens);
+		}
+		tokens = strtok_r(NULL, ",", &save_ptr);
+		if (tokens != NULL) { //R2
+			tokens = strchr(tokens, '#');
+		 	R2 = atoi(++tokens);
+		}
+		tokens = strtok_r(NULL, ",", &save_ptr);
+		if (tokens != NULL) { //R3
+			tokens = strchr(tokens, 'R');
+		 	R3 = atoi(++tokens);
+		}
+		registers[R3] = registers[R1] + R2;
+		printf("R%d + %d -> R%d\n", R1, R2, R3);
+	} else if (command == 1) { //R1 - R2 -> R3
+		int R1, R2, R3;
+		char* tokens;
+		char* save_ptr;
+		tokens = strtok_r(params, ",", &save_ptr);
+		if (tokens != NULL) { //R1
+			tokens = strchr(tokens, 'R');
+		 	R1 = atoi(++tokens);
+		}
+		tokens = strtok_r(NULL, ",", &save_ptr);
+		if (tokens != NULL) { //R2
+			tokens = strchr(tokens, 'R');
+		 	R2 = atoi(++tokens);
+		}
+		tokens = strtok_r(NULL, ",", &save_ptr);
+		if (tokens != NULL) { //R3
+			tokens = strchr(tokens, 'R');
+		 	R3 = atoi(++tokens);
+		}
+		registers[R3] = registers[R1] - registers[R2];
+		printf("R%d - R%d -> R%d\n", R1, R2, R3);
+	}
+}
+
+void printRegisters(int* registers) {
+	int i;
+	printf("\n\n***REGISTRI A FINE ESECUZIONE***\n");
+	for (i = 0; i < 64; i += 8) {
+		printf("R%d = %d\t\t", i, registers[i]);
+		printf("R%d = %d\t\t", i+1, registers[i+1]);
+		printf("R%d = %d\t\t", i+2, registers[i+2]);
+		printf("R%d = %d\t\t", i+3, registers[i+3]);
+		printf("R%d = %d\t\t", i+4, registers[i+4]);
+		printf("R%d = %d\t\t", i+5, registers[i+5]);
+		printf("R%d = %d\t\t", i+6, registers[i+6]);
+		printf("R%d = %d\n", i+7, registers[i+7]);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	FILE *inputfile;
 	char *buff, *params;
-	int cmd, code;
+	int cmd, code, i;
+	int *registers;
 	
 	if (argc != 2) { printf("Usage: %s <D-RISC file>", argv[0]); return -1;}
 	
 	inputfile = fopen(argv[1], "r");
+	registers = calloc(64, sizeof(int));
+	registers[0] = malloc(sizeof(int));
+	registers[0] = 0; //R0 contiene sempre 0
+	for (i = 1; i < 64; i++) { registers[i] = malloc(sizeof(int)); registers[i] = 0; }
 	
 	do {
 		buff = malloc(10*sizeof(char));
@@ -62,7 +149,7 @@ int main(int argc, char *argv[]) {
 		if (code != EOF && code > 0) {
 			cmd = parseCommand(buff);
 			fgets(params, 20, inputfile);
-			//printf("%d\t%s\n", cmd, params);
+			execCommand(cmd, params, registers);
 			
 			free(buff);
 			free(params);
@@ -71,6 +158,10 @@ int main(int argc, char *argv[]) {
 	
 	fclose(inputfile);
 	free(buff);
+	free(params);
+	
+	printRegisters(registers);
+	free(registers);
 	
 	return 0;
 }
