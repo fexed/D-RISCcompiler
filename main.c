0d98676b26582f5f6141cd4087125906c1ee09bb
Federico Matteoni <fedex15@live.it>
2020-01-18 22:51:26 +0100

    First steps

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+/**
+	Author: Federico Matteoni
+	Created: 18/01/2020 22.40
+*/
 int main(int argc, char *argv[]) {
+	FILE *inputfile;
+	char *buff;
+	
+	if (argc != 2) { printf("Usage: %s <D-RISC file>", argv[0]); return -1;}
+	
+	inputfile = fopen(argv[1], "r");
+	buff = malloc(50*sizeof(char));
+	fscanf(inputfile, "%s", buff);
+	printf("1. %s", buff);
+	fclose(inputfile);
+	free(buff);
 	
 	return 0;
 }
